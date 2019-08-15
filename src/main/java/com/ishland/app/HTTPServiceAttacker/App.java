28814d695542ca55242d1e4c6fe3845fdd628807
ishland <ishlandmc@yeah.net>
2019-08-15 17:08:00 +0800

    Configuration loader
    Warning: This program is not completed yet

--- src/main/java/com/ishland/app/HTTPServiceAttacker/App.java
+++ src/main/java/com/ishland/app/HTTPServiceAttacker/App.java
@@ -1,7 +1,16 @@
 package com.ishland.app.HTTPServiceAttacker;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.ishland.app.HTTPServiceAttacker.configuration.Configuration;
+
 public class App {
+    private static final Logger logger = LogManager.getLogger("Launcher");
+    private static Configuration config = null;
+
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+	logger.info("Starting HTTP Service Attacker...");
+	config = new Configuration();
     }
 }
--- src/main/java/com/ishland/app/HTTPServiceAttacker/configuration/Configuration.java
+++ src/main/java/com/ishland/app/HTTPServiceAttacker/configuration/Configuration.java
@@ -2,20 +2,51 @@ package com.ishland.app.HTTPServiceAttacker.configuration;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
 
 public class Configuration {
     private File configfile = new File(System.getProperty("user.dir") + "/config.yml");
+    private static final Logger logger = LogManager.getLogger("Configuration loader");
 
     public Configuration() {
+	logger.info("Loading configurations...");
 	FileReader reader = null;
 	try {
 	    reader = new FileReader(configfile);
 	} catch (FileNotFoundException e) {
-
+	    logger.fatal("Unable to load configuration file, creating one and exit...", e);
+	    createFile();
+	    System.exit(0);
 	}
 	YamlReader conf = new YamlReader(reader);
+    }
+
+    public void createFile() {
+	FileOutputStream out = null;
+	InputStream confin = null;
+	try {
+	    configfile.createNewFile();
+	    out = new FileOutputStream(configfile);
+	    confin = Configuration.class.getClassLoader()
+		    .getResourceAsStream("com/ishland/app/HTTPServiceAttacker/configuration/config.yml");
+	} catch (IOException e) {
+	    logger.fatal("Unable to create configuration file!", e);
+	    return;
+	}
+	try {
+	    while (confin.available() > 0)
+		out.write(confin.read());
+	} catch (IOException e) {
+	    logger.fatal("Unable to write configuration file!", e);
+	    System.exit(1);
+	}
     }
 }
